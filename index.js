ed2209831a11f824f2b3f582263362dd2a38ed73
Robert Marshall <robert.marshall@madetech.com>
2020-11-05 16:36:55 +0000

    Code change to check deploy process

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World!1!')
+    res.send('Hello World!1!1')
 })
 
 module.exports.handler = serverless(app);
